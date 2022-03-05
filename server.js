5bb8f5b746bf8c7c9dd3713edc06628bfa9cb217
unknown <bharadwajtheruvath14@gmail.com>
2022-03-05 13:09:35 +0530

    admin product endpoints created

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-const express = require('express');
-
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
+
+const adminProducts = require("./routes/admin/products");
+
+app.use(express.json());
+
+app.use(cors());
 
 //Routes
-app.get('/',(req,res) =>{
-    res.send('home');
-});
+app.use("/api/admin/products", adminProducts);
+
+//DB
+mongoose
+  .connect(
+    "mongodb+srv://thanos:T3TjzjiRJ956PMYn@cluster0.4kiky.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB..."));
 
 const PORT = 4000;
 
-app.listen(PORT,()=>console.log(`Listening to port ${PORT}`));
-
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
