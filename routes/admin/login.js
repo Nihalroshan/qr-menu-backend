fdc13021ba86ee00476a0b4a014d83c28b7eb55b
unknown <bharadwajtheruvath14@gmail.com>
2022-03-19 12:52:42 +0530

    admin login error

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const Login = require("../../models/admin/Login");
 const bcrypt = require("bcryptjs");
+const res = require("express/lib/response");
 
 //New Admin
 router.post("/new", async (req, res) => {
-  const { username, email, password} = req.body;
+  const { username, email, password } = req.body;
 
   //Hashing
   const salt = await bcrypt.genSalt(10);
@@ -22,4 +23,18 @@ router.post("/new", async (req, res) => {
     res.send({ message: err });
   }
 });
+
+//Admin login
+router.post("/", async (req, res) => {
+  //check if email exists
+  const adminEmail = await Login.findOne({ email: req.body.email });
+  if (!adminEmail) return res.status(404).send("Email doesnt exists");
+  //password is correct
+  const validPass = await bcrypt.compare(req.body.password, login.password);
+  if (!validPass)
+    return res.status(404).send("Email and password doesnt match");
+
+  res.send("logged in");
+});
+
 module.exports = router;
