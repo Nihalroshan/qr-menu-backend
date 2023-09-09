cbec250d73c8e7e91396cdcf57ad90c503282952
Nihalroshan <nihalroshanpc@gmail.com>
2023-09-09 23:00:14 +0530

    error resolved

--- routes/categories.js
+++ routes/categories.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const adminAuth = require("../middlewares/adminAuth");
 const router = express.Router();
-const Category = require("../models/Category");
+const Category = require("../models/category");
 
 //Get all category details
 router.get("/", async (req, res) => {
